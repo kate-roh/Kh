59a52ae4eb2a94393961504deb3e1e1f8e857963
kate-roh <52918202+kate-roh@users.noreply.github.com>
2021-07-05 04:14:30 +0900

    Update Test.java

--- Java03_Operator/src/com/practice/Test.java
+++ Java03_Operator/src/com/practice/Test.java
@@ -1,5 +1,15 @@
+//6.18 숙제 & 6.21 수업
+//7.5 복습
+
 package com.practice;
 
 public class Test {
+	public static void main(String[] args) {
+		Example ex = new Example();
+		ex.sample1();
+		ex.sample2();
+		ex.sample3();
+		ex.sample4();
+	}
 
 }
