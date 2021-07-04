2160ccbdb6bb9ff4a64fa0c03ac23b36aea32713
kate-roh <52918202+kate-roh@users.noreply.github.com>
2021-07-05 06:40:00 +0900

    Update and rename TestMain to TestMain.java

--- Java05_Switch/src/com/test01/TestMain.java
+++ Java05_Switch/src/com/test01/TestMain.java
@@ -6,7 +6,7 @@ public class TestMain {
 
 	public static void main(String[] args) {
 		//준비 
-		//클래스명 변수명 = new 
+		//클래스명 변수명 = new 클래스명();
 		SwitchTest01 st = new SwitchTest01;
 				
 		//실행
@@ -17,6 +17,7 @@ public class TestMain {
 //		st.test4();
 		st.test5();
 		
+		new SwitchTest02().testSwitch();
 	}
 
 }
