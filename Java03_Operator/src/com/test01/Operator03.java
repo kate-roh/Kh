0a65c41e341501ee05e10243be009582fd383da8
kate-roh <52918202+kate-roh@users.noreply.github.com>
2021-07-05 02:31:45 +0900

    Update Operator03.java

--- Java03_Operator/src/com/test01/Operator03.java
+++ Java03_Operator/src/com/test01/Operator03.java
@@ -6,7 +6,8 @@ import java.util.Scanner;
 public class Operator03 {
 	public static void main(String[] args) {
 		Operator03 test = new Operator03();
-		test.opTest1();
+		//test.opTest1();
+		test.opTest2();
 	}
 
 	// &(그리고, ~면서)
@@ -36,6 +37,13 @@ public class Operator03 {
 	
 	}
 	
+	public void opTest2() {
+		int num = 10;
+		
+ 		int res = ( false || ++num == 0)? num : num;
+		System.out.println("&&확인: " + num);
+		System.out.println("res");
+	}
 	
 	
 }
