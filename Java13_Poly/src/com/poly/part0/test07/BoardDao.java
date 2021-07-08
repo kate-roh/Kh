4774185f5ce48ccbe10329b10c61d5201289775c
kate-roh <52918202+kate-roh@users.noreply.github.com>
2021-07-08 12:32:50 +0900

    Add files via upload

---
+++ Java13_Poly/src/com/poly/part0/test07/BoardDao.java
@@ -0,0 +1,7 @@
+package com.poly.part0.test07;
+
+public class BoardDao {
+	
+	public 
+
+}
---
+++ Java13_Poly/src/com/poly/part0/test07/Job.java
@@ -0,0 +1,8 @@
+//7.08 2교시 수업
+package com.poly.part0.test07;
+
+public interface Job {
+
+	void jobLoc(String loc);
+
+}
