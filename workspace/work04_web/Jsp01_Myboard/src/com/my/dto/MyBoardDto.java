64811dc6f90686c0bb237ed20a4e10219985068e
kate-roh <52918202+kate-roh@users.noreply.github.com>
2021-09-16 14:53:37 +0900

    update

---
+++ workspace/work04_web/Jsp01_Myboard/src/com/my/dto/MyBoardDto.java
@@ -0,0 +1,15 @@
+package com.my.dto;
+
+	private int myno;
+	private String myname;
+	private String mytitle;
+	private String 
+
+
+public class MyBoardDto {
+	super();
+}
+public MyBoardDto(int myno, String myname, String mytitle, String mycontent, Date mydate) {
+	super();
+	this
+}
