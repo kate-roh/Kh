11a2264cb4027047a5e04308a4b74ce5a505d3e0
kate-roh <52918202+kate-roh@users.noreply.github.com>
2021-10-14 15:34:35 +0900

    Delete Java13_Poly directory

--- Java13_Poly/src/com/poly/part0/test07/BoardDao.java
+++
@@ -1,7 +0,0 @@
-package com.poly.part0.test07;
-
-public class BoardDao {
-	
-	public 
-
-}
--- Java13_Poly/src/com/poly/part0/test07/Display.java
+++
@@ -1,7 +0,0 @@
-//7.08 2교시 수업
-package com.poly.part0.test07;
-
-public interface Display {
-	public void display();
-	
-}
